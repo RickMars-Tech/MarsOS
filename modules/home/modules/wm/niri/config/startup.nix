ddadddb45fbb6cd5f31706c883c9004e4bece3d2
RickMars-Tech <rickmars117@proton.me>
2025-08-07 11:53:41 -0600

    small adjustments

--- modules/home/modules/wm/niri/config/startup.nix
+++ modules/home/modules/wm/niri/config/startup.nix
@@ -1,17 +1,24 @@
-{pkgs, ...}: let
+{
+  config,
+  pkgs,
+  ...
+}: let
   makeCommand = command: {
     command = [command];
   };
+  wall = config.stylix.image;
 in {
   programs.niri.settings.spawn-at-startup = [
     (makeCommand "systemctl --user import-environment")
     (makeCommand "systemctl --user restart pipewire.service")
-    (makeCommand "systemctl --user restart xdg-desktop-portal.service")
+    (makeCommand "systemctl --user restart xdg-desktop-portal-gnome.service")
     (makeCommand "systemctl --user restart iwgtk.service")
     # (makeCommand "systemctl --user restart blueman-applet.service")
     (makeCommand "wl-clip-persist --clipboard regular")
     (makeCommand "systemctl --user start cliphist.service")
     (makeCommand "xprop -root -f _XWAYLAND_GLOBAL_OUTPUT_SCALE 32c -set _XWAYLAND_GLOBAL_OUTPUT_SCALE 2")
+    (makeCommand "swww-daemon")
+    (makeCommand "swww img ${wall}")
     (makeCommand "$POLKIT_BIN")
     (makeCommand "wlr-randr")
     (makeCommand "dunst")
