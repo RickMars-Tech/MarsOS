41e5792b9574a3f9251b50015daf587667fbcbca
RickMars-Tech <rickmars117@proton.me>
2025-08-21 10:07:20 -0600

    UWSM Implemented

--- modules/system/desktop/sessionManager.nix
+++ modules/system/desktop/sessionManager.nix
@@ -1,12 +1,23 @@
 {pkgs, ...}: let
   tuigreet = "${pkgs.tuigreet}/bin/tuigreet";
+  niri-session = "${pkgs.niri}/bin/niri";
 in {
-  #==< TuiGreet >==#
+  #|==< UWSM >==|#
+  programs.uwsm = {
+    enable = true;
+    waylandCompositors.niri = {
+      prettyName = "Niri";
+      comment = "Scrollable-tiling Wayland compositor";
+      binPath = niri-session;
+    };
+  };
+
+  #|==< TuiGreet >==|#
   services.greetd = {
     enable = true;
     settings = {
       default_session = {
-        command = "${tuigreet} --time --remember --asterisks --container-padding 2 --no-xsession-wrapper --cmd niri-session";
+        command = "${tuigreet} --time --remember --asterisks --container-padding 2 --no-xsession-wrapper";
         user = "greeter";
       };
     };
