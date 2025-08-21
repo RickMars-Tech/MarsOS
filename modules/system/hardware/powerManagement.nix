8af46e97f6527a88723de46c50b131d0bd71760e
RickMars-Tech <rickmars117@proton.me>
2025-08-21 10:05:05 -0600

    RCU Script/Service Implemented

--- modules/system/hardware/powerManagement.nix
+++ modules/system/hardware/powerManagement.nix
@@ -1,18 +1,26 @@
 {
   config,
+  pkgs,
   lib,
   ...
 }: let
   inherit (lib) mkIf mkEnableOption;
+  rcu-power-manager = pkgs.callPackage ../../../pkgs/gamingScripts/rcu-power-manager.nix {};
   cfg = config.mars;
 in {
   options.mars.laptopOptimizations = mkEnableOption "Laptop Optimizations";
 
-  config = mkIf (cfg.laptopOptimizations && cfg.laptopOptimizations) {
+  config = mkIf cfg.laptopOptimizations {
     # Lid
     services.logind.lidSwitch = "poweroff";
     services.logind.lidSwitchExternalPower = "lock";
     services.logind.lidSwitchDocked = "ignore";
+
+    #= RCU
+    boot.kernelParams = [
+      "rcutree.enable_rcu_lazy=1"
+    ];
+    environment.systemPackages = [rcu-power-manager];
 
     # UPower
     services.upower.enable = true;
@@ -38,9 +46,20 @@ in {
         # Optional helps save long term battery health
         START_CHARGE_THRESH_BAT0 = 40; # 40 and bellow it starts to charge
         STOP_CHARGE_THRESH_BAT0 = 80; # 80 and above it stops charging
-        TLP_DEFAULT_MODE = "BAT";
-        TLP_PERSISTENT_DEFAULT = 1;
+        # TLP_DEFAULT_MODE = "BAT";
+        # TLP_PERSISTENT_DEFAULT = 1;
       };
+    };
+    powerManagement = {
+      enable = true;
+      powerUpCommands = ''
+        sleep 2
+        echo 0 > /sys/module/rcutree/parameters/enable_rcu_lazy 2>/dev/null || true
+      '';
+      powerDownCommands = ''
+        sleep 2
+        echo 1 > /sys/module/rcutree/parameters/enable_rcu_lazy 2>/dev/null || true
+      '';
     };
   };
 }
