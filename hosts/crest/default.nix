a10c9396efc91c51a528fb249ac6401bff5bf8da
RickMars-Tech <rickmars117@proton.me>
2025-08-25 22:19:48 -0600

    New Host

---
+++ hosts/crest/default.nix
@@ -0,0 +1,45 @@
+#|==< Configuration for Asus (Ryzen + Nvidia Laptop) >==|#
+_: {
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware.nix
+    ../../modules/system/default.nix
+  ];
+
+  # Hostname
+  networking.hostName = "rift";
+
+  #|==< Mars Config >==|#
+  mars = {
+    cpu.amd.enable = true;
+    graphics = {
+      enable = true;
+      amd = {
+        enable = true;
+        vulkan = true;
+        opengl = true;
+        compute.enable = false;
+      };
+    };
+    gaming = {
+      enable = true;
+      gamemode = {
+        enable = true;
+        amdOptimizations = true;
+        nvidiaOptimizations = false;
+      };
+      minecraft = {
+        prismlauncher.enable = true;
+        extraJavaPackages.enable = true;
+      };
+      steam = {
+        enable = true;
+        openFirewall = false;
+        hardware-rules = true;
+      };
+      extra-gaming-packages = true;
+    };
+  };
+
+  system.stateVersion = "25.11";
+}
