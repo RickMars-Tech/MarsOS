a17491b9f8cd514f1beecc695d01be09ea50a458
RickMars-Tech <rickmars117@proton.me>
2025-08-25 22:31:54 -0600

    Environment Variables Changes/Improvements

--- modules/system/desktop/env.nix
+++ modules/system/desktop/env.nix
@@ -4,43 +4,85 @@
   lib,
   ...
 }: let
-  inherit (lib) getExe mkIf;
-  nvidia = config.mars.graphics.nvidiaPro;
+  inherit (lib) getExe mkIf mkMerge;
+  #= Options
+  nvidiaPro = config.mars.graphics.nvidiaPro;
+  nvidiaFree = config.mars.graphics.nvidiaFree;
   amd = config.mars.graphics.amd;
+  gaming = config.mars.gaming.gamemode;
+  steam = config.mars.gaming.steam;
+  minecraft = config.mars.gaming.minecraft.prismlauncher;
+  #= Packages
   firefox = "${pkgs.firefox}/bin/firefox";
   terminal = "${pkgs.wezterm}/bin/wezterm";
   soteria = getExe pkgs.soteria;
-  steam = config.mars.gaming.steam;
-  minecraft = config.mars.gaming.minecraft.prismlauncher;
 in {
   environment = {
-    sessionVariables = {
-      #|==< Default's >==|#
-      EDITOR = "hx";
-      BROWSER = "${firefox}";
-      TERMINAL = "${terminal}";
+    sessionVariables = mkMerge [
+      {
+        #|==< Default's >==|#
+        EDITOR = "hx";
+        BROWSER = "${firefox}";
+        TERMINAL = "${terminal}";
 
-      #|==< Polkit >==|#
-      POLKIT_BIN = "${soteria}";
+        #|==< Polkit >==|#
+        POLKIT_BIN = "${soteria}";
+      }
+      #|==< AMD >==|#
+      (mkIf amd.enable {
+        # https://reddit.com/r/linux_gaming/comments/1mg8vtl/low_latency_gaming_guide/
+        MESA_VK_WSI_PRESENT_MODE = mkIf (gaming.amdOptimizations && amd.enable) "fifo";
 
-      #|==< ShaderOptimizations >==|#
-      AMD_VULKAN_ICD = mkIf amd.enable "RADV";
-      MESA_SHADER_CACHE_MAX_SIZE = mkIf amd.enable "12G";
-      __GL_SHADER_DISK_CACHE_SIZE = mkIf nvidia.enable 12000000000;
+        #= ShaderOptimizations
+        # https://github.com/psygreg/shader-booster/
+        AMD_VULKAN_ICD = "RADV";
+        MESA_SHADER_CACHE_MAX_SIZE = mkIf (gaming.amdOptimizations && amd.enable) "12G";
 
-      #|==< nVidiaPrime >==|#
-      GAMEMODERUNEXEC = mkIf nvidia.enable "nvidia-offload";
+        RADV_PERFTEST = "gpl,ngg,sam,rt";
+      })
 
-      #|==< JAVA >==|#
-      _JAVA_AWT_WM_NONREPARENTING = mkIf minecraft.enable "1";
+      #= AMD Compute Environment
+      (mkIf (amd.compute.enable && amd.compute.rocm) {
+        # ROCm environment
+        ROCM_PATH = "${pkgs.rocmPackages.clr}";
+        HIP_PATH = "${pkgs.rocmPackages.hip-common}";
+      })
 
-      #|==< Load Shared Objects Immediately >==|#
-      LD_BIND_NOW = mkIf steam.enable "1";
+      #|==< nVidiaFree >==|#
+      (mkIf nvidiaFree.enable {
+        __GLX_VENDOR_LIBRARY_NAME = "mesa";
+        #= Force Zink on Nouveau
+        MESA_LOADER_DRIVER_OVERRIDE = mkIf nvidiaFree.zink "zink";
+
+        #= Use Nvidia GPU with FeralGamemode
+        GAMEMODERUNEXEC = mkIf (gaming.nvidiaOptimizations && nvidiaFree.enable) "DRI_PRIME=1";
+      })
+
+      #|==< nVidiaPRO >==|#
+      (mkIf nvidiaPro.enable {
+        #= ShaderOptimizations
+        # https://github.com/psygreg/shader-booster/
+        __GL_SHADER_DISK_CACHE_SIZE = 12000000000;
+        __GL_SHADER_DISK_CACHE = "1";
+        __GL_SHADER_DISK_CACHE_SKIP_CLEANUP = "1";
+
+        #= Prime
+        GAMEMODERUNEXEC = mkIf (gaming.nvidiaOptimizations && nvidiaPro.enable) "prime-run";
+      })
 
       #|==< Steam >==|#
-      STEAM_EXTRA_COMPAT_TOOLS_PATHS = mkIf steam.enable "$HOME/.steam/root/compatibilitytools.d";
-      # https://wiki.cachyos.org/configuration/gaming/#fix-stuttering-caused-by-the-steam-game-recorder-feature
-      LD_PRELOAD = mkIf steam.enable "";
-    };
+      (mkIf steam.enable {
+        STEAM_EXTRA_COMPAT_TOOLS_PATHS = "$HOME/.steam/root/compatibilitytools.d";
+        # https://wiki.cachyos.org/configuration/gaming/#fix-stuttering-caused-by-the-steam-game-recorder-feature
+        LD_PRELOAD = "";
+        #= Load Shared Objects Immediately
+        LD_BIND_NOW = "1";
+      })
+
+      #|==< JAVA >==|#
+      (mkIf minecraft.enable {
+        _JAVA_AWT_WM_NONREPARENTING = "1";
+      })
+    ];
   };
 }
