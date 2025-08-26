03269aec9fef09f0558d8167a8be727c42e8f01b
RickMars-Tech <rickmars117@proton.me>
2025-08-25 22:24:02 -0600

    RCU Power Manager Deprecated and Minimal TLP Changes/Improvements

--- modules/system/hardware/powerManagement.nix
+++ modules/system/hardware/powerManagement.nix
@@ -1,26 +1,18 @@
 {
   config,
-  pkgs,
   lib,
   ...
 }: let
   inherit (lib) mkIf mkEnableOption;
-  rcu-power-manager = pkgs.callPackage ../../../pkgs/gamingScripts/rcu-power-manager.nix {};
   cfg = config.mars;
 in {
   options.mars.laptopOptimizations = mkEnableOption "Laptop Optimizations";
 
   config = mkIf cfg.laptopOptimizations {
     # Lid
-    services.logind.lidSwitch = "poweroff";
+    services.logind.lidSwitch = "hybrid-sleep";
     services.logind.lidSwitchExternalPower = "lock";
     services.logind.lidSwitchDocked = "ignore";
-
-    #= RCU
-    boot.kernelParams = [
-      "rcutree.enable_rcu_lazy=1"
-    ];
-    environment.systemPackages = [rcu-power-manager];
 
     # UPower
     services.upower.enable = true;
@@ -43,23 +35,19 @@ in {
         CPU_MIN_PERF_ON_BAT = 0;
         CPU_MAX_PERF_ON_BAT = 20;
 
+        # Wifi
+        WIFI_PWR_ON_AC = "off";
+        WIFI_PWR_ON_BAT = "on";
+        WOL_DISABLE = "Y";
+
         # Optional helps save long term battery health
         START_CHARGE_THRESH_BAT0 = 40; # 40 and bellow it starts to charge
         STOP_CHARGE_THRESH_BAT0 = 80; # 80 and above it stops charging
+        START_CHARGE_THRESH_BAT1 = 40; # 40 and bellow it starts to charge
+        STOP_CHARGE_THRESH_BAT1 = 80; # 80 and above it stops charging
         # TLP_DEFAULT_MODE = "BAT";
         # TLP_PERSISTENT_DEFAULT = 1;
       };
-    };
-    powerManagement = {
-      enable = true;
-      powerUpCommands = ''
-        sleep 2
-        echo 0 > /sys/module/rcutree/parameters/enable_rcu_lazy 2>/dev/null || true
-      '';
-      powerDownCommands = ''
-        sleep 2
-        echo 1 > /sys/module/rcutree/parameters/enable_rcu_lazy 2>/dev/null || true
-      '';
     };
   };
 }
