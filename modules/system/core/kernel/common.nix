574ead8e882e0243fbcd83e9a280f9d6a7ffd0e3
RickMars-Tech <rickmars117@proton.me>
2025-08-31 19:48:58 -0600

    zswap config added

--- modules/system/core/kernel/common.nix
+++ modules/system/core/kernel/common.nix
@@ -23,6 +23,11 @@ in {
         #"iomem=relaxed"
         #= Vulnerability mitigations
         "mitigations=auto"
+        #= ZSwap
+        "zswap.enabled=1" # enables zswap
+        "zswap.compressor=zstd" # compression algorithm
+        "zswap.max_pool_percent=20" # maximum percentage of RAM that zswap is allowed to use
+        "zswap.shrinker_enabled=1" # whether to shrink the pool proactively on high memory pressure
       ]
       ++ optionals plymouth.enable [
         #= Silent Mode
