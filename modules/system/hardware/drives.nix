b0ef03f1b0e88b53a5ae2a00fa2bfef20a6c490c
RickMars-Tech <rickmars117@proton.me>
2025-08-31 20:02:42 -0600

    BTRFS Improvements and some Changes

--- modules/system/hardware/drives.nix
+++ modules/system/hardware/drives.nix
@@ -4,28 +4,51 @@
   lib,
   ...
 }: let
-  inherit (lib) mkIf mkDefault;
-  # Verificar específicamente si el sistema root (/) usa BTRFS
+  inherit (lib) mkIf mkDefault mkMerge optionals;
+  #= Detect if Root is BTRFS
   rootIsBtrfs = config.fileSystems."/".fsType or "" == "btrfs";
+
+  #= Detect if is SDD
+  hasSSD =
+    builtins.pathExists /sys/block
+    && builtins.any (dev:
+      builtins.pathExists "/sys/block/${dev}/queue/rotational"
+      && builtins.readFile "/sys/block/${dev}/queue/rotational" == "0\n")
+    (builtins.attrNames (builtins.readDir /sys/block));
 in {
   #|==< FileSystem >==|#
-  fileSystems = {
-    #= BTRFS Optimizations
-    "/".options = mkIf rootIsBtrfs [
-      "ssd"
-      "compress=zstd:6"
-      "commit=120"
-      "noatime"
-      "discard=async"
-      "space_cache=v2"
-    ];
-    #= Tmp Partition
-    "/tmp" = {
-      device = "tmpfs";
-      fsType = "tmpfs";
-      options = ["size=3G" "noatime" "mode=1777"]; # Use 8G if you have 32GB of ram or more.
-    };
-  };
+  fileSystems = mkMerge [
+    {
+      #= BTRFS Optimizations
+      "/".options = mkIf rootIsBtrfs ([
+          "compress=zstd:6"
+          "relatime"
+          "commit=120"
+          "discard=async"
+          "space_cache=v2"
+        ]
+        ++ (
+          if hasSSD
+          then [
+            "ssd"
+          ]
+          else [
+            "autodefrag"
+          ]
+        ));
+      #= Tmp Partition
+      "/tmp" = {
+        device = "tmpfs";
+        fsType = "tmpfs";
+        options = [
+          "size=4G"
+          "noatime"
+          "noexec"
+          "mode=1777"
+        ];
+      };
+    }
+  ];
   # Enable AutoScrub if Root Filesystem is BTRFS
   services.btrfs.autoScrub = {
     enable = mkIf rootIsBtrfs true;
@@ -34,9 +57,9 @@ in {
   };
 
   #= Enable Trim (Needed for SSD's).
-  services.fstrim = mkIf (!rootIsBtrfs) {
+  services.fstrim = {
     enable = mkDefault true;
-    interval = "weekly";
+    interval = mkIf rootIsBtrfs "monthly"; # Less frecuent on BTRFS by discard=async
   };
 
   #= ZRAM
@@ -53,7 +76,7 @@ in {
   # Deduplication of identical memory pages
   hardware.ksm = {
     enable = mkDefault true;
-    sleep = mkDefault 20; # Run every 20 seconds (balance between CPU and memory)
+    sleep = mkDefault 20;
   };
 
   #= Prevents system freezes due to lack of memory
@@ -70,7 +93,21 @@ in {
     ];
   };
 
-  environment.systemPackages = with pkgs; mkIf rootIsBtrfs [compsize];
+  environment.systemPackages = with pkgs;
+    [
+      caligula # User-friendly, lightweight TUI for disk imaging
+      gnome-disk-utility # Disk Manager.
+      baobab # Gui app to analyse disk usage.
+      woeusb # Flash OS images for Windows.
+    ]
+    ++ optionals rootIsBtrfs [
+      compsize
+      btrfs-progs
+    ]
+    ++ optionals hasSSD [
+      hdparm
+      smartmontools
+    ];
 
   #= Allows Applications to query and manipulate Storage Devices
   services.udisks2.enable = true;
