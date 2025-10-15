eebba2904407e7140bc1df0b4d42ab77377d2bb5
RickMars-Tech <rickmars117@proton.me>
2025-10-15 02:03:35 -0600

    Simplified "Dinamic" Imports

--- modules/home/windowManager/default.nix
+++ modules/home/windowManager/default.nix
@@ -1,19 +1,9 @@
 {lib, ...}: let
-  inherit (builtins) filter map readDir pathExists;
-  inherit (lib.attrsets) filterAttrs mapAttrsToList;
+  inherit (builtins) filter map readDir pathExists attrNames;
+  isDirectory = _: type: type == "directory";
+  hasDefault = name: pathExists (./. + "/${name}/default.nix");
 in {
   imports =
     map (name: ./. + "/${name}")
-    (
-      filter (
-        name:
-        # Verifica que existe un default.nix en el subdirectorio
-          pathExists (./. + "/${name}/default.nix")
-      )
-      (
-        # Solo nombres de directorios (no archivos)
-        mapAttrsToList (name: type: name)
-        (filterAttrs (name: type: type == "directory") (readDir ./.))
-      )
-    );
+    (filter hasDefault (attrNames (lib.filterAttrs isDirectory (readDir ./.))));
 }
