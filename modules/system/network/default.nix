bd8c32aee90c952454e03973661b65e19ce823db
RickMars-Tech <rickmars117@proton.me>
2026-09-08 10:24:30 -0600

    Regresion to last Network config

--- modules/system/network/default.nix
+++ modules/system/network/default.nix
@@ -25,9 +25,15 @@
           settings.Resolve = {
             Domains = [ "~." ];
 
-            DNSOverTLS = true;
+            # DNSOverTLS para mayor privacidad y seguridad
+            dnsovertls = "opportunistic";
 
             fallbackDns = [
+              #= Cloudflare (Block Malware)
+              "1.1.1.2"
+              "2606:4700:4700::1112"
+              "1.0.0.2"
+              "2606:4700:4700::1002"
               #= Quad9 (Alternativa segura y privada)
               "9.9.9.9"
               "2620:fe::fe"
@@ -35,22 +41,24 @@
               "8.8.8.8"
               "2001:4860:4860::8888"
             ];
+
+            dnssec = "allow-downgrade";
+
+            MulticastDNS = true;
+            LLMNR = true;
+            ReadEtcHosts = true;
+            ResolveUnicastSingleLabel = false;
+            Cache = true;
+            CacheFromLocalhost = false;
+            DNSStubListener = true;
+            DNSStubListenerExtra = "127.0.0.53";
           };
         };
 
-        # Segurity
+        #== Segurity
         openssh.enable = mkDefault false;
         fail2ban.enable = mkDefault false;
         samba.enable = false;
       };
-
-      networking.nameservers = [
-        "179.9.93.198" # dnsforge.de
-        "179.9.1.117"
-
-        # IPv6
-        "2a01:4f8:151:34aa::198"
-        "2a01:4f8:141:316d::117"
-      ];
     };
 }
